d5e8b62cfb03c6699bc07181a785d7c99e47cfbc
IAMLLT796 <lt.liu796@gmail.com>
2023-05-15 23:39:58 +0800

    aop的xml配置测试

--- s002-spring-framework-demo-aop-xml/src/main/java/tech/llt/springframework/App.java
+++ s002-spring-framework-demo-aop-xml/src/main/java/tech/llt/springframework/App.java
@@ -1,5 +1,9 @@
 package tech.llt.springframework;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import tech.llt.springframework.service.AopDemoServiceImpl;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //创建和配置bean
+        ApplicationContext context=new ClassPathXmlApplicationContext(("aspect.xml"));
+
+        //检索配置实例
+        AopDemoServiceImpl service=context.getBean("demoService",AopDemoServiceImpl.class);
+
+        //使用配置实例
+        service.doMethod1();
+        service.doMethod2();
+
+        try {
+            service.doMethod3();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
